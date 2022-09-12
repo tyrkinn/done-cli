a385ee402f4c8f08769fa131ea5539cc3991a32b
tyrkinn <nikemad61@gmail.com>
2022-09-13 01:26:01 +0300

    feat: add logging to install and update scripts

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #! /bin/sh
 
-if [ "$(which git)" = "" ];
+echo "Checking git..."
+
+if [[ "$(which git)" = "" ]] || [[ "$(which git)" = "git not found" ]];
 then
   echo "Install git first"
   exit 0
 fi
+
+echo "Cloning repo..."
 
 git clone https://gitlab.com/tyrkinn/done-cli/
 
 cd done-cli
 
 bbpath=$(which bb)
+
+echo "Checking babashka..."
 
 if [[ "$bbpath" = "" ]] || [[ "$bbpath" = "bb not found" ]];
 then
@@ -21,17 +27,27 @@ then
   rm -rf ./install
 fi
 
+echo "Moving done! to /usr/local/bin ..."
+
 cp ./done\!.clj /usr/local/bin/done\!
 
+echo "Moving update-done util to /usr/local/bin ..."
+
 cp ./update-done.sh /usr/local/bin/update-done
+
+echo "Giving execute permissons to update-done ..."
 
 chmod +x /usr/local/bin/update-done
 
 cd ..
 
+echo "Removing repo"
+
 rm -rf done-cli
 
-echo "Done!-cli successfully moved to bin.\nType \`done!\` to see usage string"
+echo "\nDone!-cli successfully moved to /usr/local/bin.\n\nType \`done!\` to see usage string\n"
+
+echo "Cleanup ..."
 
 function finish {
   shred -u install.sh;
--- update-done.sh
+++ update-done.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+echo "Updating done cli ..."
 curl https://gitlab.com/tyrkinn/done-cli/-/raw/main/done!.clj > /usr/local/bin/done!
+echo "New your done! up to date!"

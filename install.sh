59b6efae1ba0c6f95f37100059b9fe29c1ff95ba
tyrkinn <nikemad61@gmail.com>
2022-09-12 16:51:19 +0300

    feat: update install.sh to check bb

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
 #! /bin/zsh
 
-mv ./done\!.clj /usr/local/bin/done!
+bbpath=$(which bb)
+
+if [ $bbpath = "bb not found" ];
+then
+  echo "Installing babashka\n"
+  curl -sLO https://raw.githubusercontent.com/babashka/babashka/master/install
+  chmod +x install
+  ./install
+  rm -rf ./install
+fi
+
+cp ./done\!.clj /usr/local/bin/done\!
+
+echo "Done!-cli successfully moved to bin.\nType \`done!\` to see usage string"
+

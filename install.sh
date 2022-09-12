c4a323852ba56cc975e8dc4440d78bedf90cccf0
tyrkinn <nikemad61@gmail.com>
2022-09-12 23:17:24 +0300

    fix: another dumb error

---
+++ install.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+if [ "$(which git)" = "" ];
+then
+  echo "Install git first"
+  exit 0
+fi
+
+git clone https://gitlab.com/tyrkinn/done-cli/
+
+cd done-cli
+
+bbpath=$(which bb)
+
+if [[ "$bbpath" = "" ]] || [[ "$bbpath" = "bb not found" ]];
+then
+  echo "Installing babashka\n"
+  curl -sLO https://raw.githubusercontent.com/babashka/babashka/master/install
+  chmod +x install
+  ./install
+  rm -rf ./install
+fi
+
+cp ./done\!.clj /usr/local/bin/done\!
+
+cp ./update-done.sh /usr/local/bin/update-done
+
+chmod +x /usr/local/bin/update-done
+
+cd ..
+
+rm -rf done-cli
+
+echo "Done!-cli successfully moved to bin.\nType \`done!\` to see usage string"
+
+function finish {
+  shred -u install.sh;
+}
+
+trap finish EXIT
